1f479d4635a1d8df8202bb463134df62e92420c4
홍현정 <emilyhong4659@gmail.com>
2024-11-12 17:25:40 +0900

    [FE] fix : Navirouter 수정

--- client/src/Navirouter.js
+++ client/src/Navirouter.js
@@ -1,22 +1,51 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/common/HomePage";
-import SaveForm from "./components/pages/child/plan/SaveForm";
-import UpdateForm from "./components/pages/child/plan/UpdateForm";
-import PlanPage from "./pages/child/PlanPage";
-import SendTest from "./pages/test/child/SendTest";
-import ParentTest from "./pages/test/parent/ParentTest";
+// import SaveForm from "./components/pages/child/plan/SaveForm";
+// import UpdateForm from "./components/pages/child/plan/UpdateForm";
+// import PlanPage from "./pages/child/PlanPage";
+// import SendTest from "./pages/test/child/SendTest";
+// import ParentTest from "./pages/test/parent/ParentTest";
+
+import LandingPage from "./pages/common/LandingPage";
+import ChildReportPage from "./pages/child/ChildReportPage";
+import EduPage from "./pages/child/EduPage";
+import MoneyPlanPage from "./pages/child/MoneyPlanPage";
+import MyWishListPage from "./pages/child/MyWishListPage";
+import ChildRegisterPage from "./pages/child/RegisterPage";
+import AgreementPage from "./pages/parent/AgreementPage";
+import MonthlyReportPage from "./pages/parent/MonthlyReportPage";
+import WishListPage from "./pages/parent/WishListPage";
+import RegisterPage from "./pages/parent/RegisterPage";
+import LoginPage from "./pages/common/LoginPage";
+import SelectUserType from "./pages/common/SelectUserType";
 
 const Navirouter = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/save-form" element={<SaveForm />} />
+        {/* 공통 */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/select-user" element={<SelectUserType />} />
+
+        {/* 아이 페이지 */}
+        <Route path="/child-report" element={<ChildReportPage />} />
+        <Route path="/edu" element={<EduPage />} />
+        <Route path="/money-plan" element={<MoneyPlanPage />} />
+        <Route path="/mywish-list" element={<MyWishListPage />} />
+        <Route path="/register-child" element={<ChildRegisterPage />} />
+
+        {/* 부모 페이지 */}
+        <Route path="/agreement" element={<AgreementPage />} />
+        <Route path="/monthly-report" element={<MonthlyReportPage />} />
+        <Route path="/wish-list" element={<WishListPage />} />
+        <Route path="/register-parent" element={<RegisterPage />} />
+
+        {/* <Route path="/save-form" element={<SaveForm />} />
         <Route path="/update-form" element={<UpdateForm />} />
-        <Route path="/plan-page" element={<PlanPage />} />
-        <Route path="/test-child" element={<SendTest />} />
-        <Route path="/test-parent" element={<ParentTest />} />
+        <Route path="/plan" element={<PlanPage />} /> */}
+        {/* <Route path="/test-child" element={<SendTest />} />
+        <Route path="/test-parent" element={<ParentTest />} /> */}
       </Routes>
     </div>
   );
