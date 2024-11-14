70c3832e0621967ee7e6b91a9b25a23d46987621
홍현정 <emilyhong4659@gmail.com>
2024-11-14 17:42:30 +0900

    [FE] feat : 부모페이지 아이,날짜 조회 navbar 추가

--- client/src/pages/parent/MonthlyReportPage.jsx
+++ client/src/pages/parent/MonthlyReportPage.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import PlanReport from "../../components/pages/parent/monthly-report/PlanReport";
+import SelectOptionNav from "../../components/pages/parent/SelectOptionNav";
 
 const MonthlyReportPage = () => {
+  const [selectOption, setSelectOption] = useState({
+    childNum: 1,
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  });
+
+  const handleChildData = (updateData) => {
+    console.log("data : ", updateData);
+
+    setSelectOption((prev) => ({
+      ...prev,
+      ...updateData, // 기존 데이터에 새로 전달된 값 병합
+    }));
+  };
+
   return (
     <div>
-      <PlanReport />
+      <SelectOptionNav onHandleData={handleChildData} />
+      {/* 선택한 옵션값 확인 (지워두 됨) */}
+      <p>ChildNum : {selectOption.childNum}</p>
+      <p>year: {selectOption.year}</p>
+      <p>month : {selectOption.month}</p>
+
+      {/* <PlanReport /> */}
     </div>
   );
 };
