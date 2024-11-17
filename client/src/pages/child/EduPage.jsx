782a5d868a4a0735cd29af1eb234284795b3a438
Heeju Lee <heejulia21@naver.com>
2024-11-17 15:35:28 +0900

    #73[FE] feat : 아이 교육 영상페이지 기능 구현완료

--- client/src/pages/child/EduPage.jsx
+++ client/src/pages/child/EduPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import EduNav from "../../components/pages/child/edu/EduNav";
+import EduVideo from "../../components/pages/child/edu/EduVideo";
+import EduQuiz from "../../components/pages/child/edu/EduQuiz";
 
 const EduPage = () => {
-  return <div>경제 배우기 페이지</div>;
+  const [selectedCategory, setSelectedCategory] = useState("영상교육"); // 선택된 카테고리
+
+  const renderContent = () => {
+    switch (selectedCategory) {
+      case "퀴즈":
+        return <EduQuiz />;
+      case "영상교육":
+      default:
+        return <EduVideo />;
+    }
+  };
+
+  return (
+    <>
+      <EduNav onSelect={setSelectedCategory} />
+      {renderContent()}
+    </>
+  );
 };
 
 export default EduPage;
